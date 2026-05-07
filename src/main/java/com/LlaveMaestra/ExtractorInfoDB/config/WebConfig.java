3af975a39efbca266da144ab2f5a5565ec814cec
maximiliano-andres <max.andres1995@outlook.com>
2026-05-07 13:26:27 -0400

    feat: mejoras de front

--- src/main/java/com/LlaveMaestra/ExtractorInfoDB/config/WebConfig.java
+++ src/main/java/com/LlaveMaestra/ExtractorInfoDB/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.LlaveMaestra.ExtractorInfoDB.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,6 +11,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     private final DataSourceInterceptor dataSourceInterceptor;
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/manual").setViewName("forward:/manual.html");
+        registry.addViewController("/ayuda").setViewName("forward:/manual.html");
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
